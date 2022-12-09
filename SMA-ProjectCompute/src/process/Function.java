38a542d7a916f62693e73186303d61c9589fd519
aladd <aladdineben@outlook.com>
2022-12-09 16:04:51 +0100

    [Feature] Adding elementary classes with elementary prototypes.

--- SMA-ProjectCompute/src/process/Function.java
+++ SMA-ProjectCompute/src/process/Function.java
@@ -1,5 +1,17 @@
 package process;
 
-public class Function {
-
+public abstract class Function {
+	private double min;
+	private double max;
+	private double delta;
+	
+	public Function(double min, double max, double delta) {
+		this.min = min;
+		this.max = max;
+		this.delta = delta;
+	}
+	
+	public abstract double eval();
+	public abstract double f(double x);
+	
 }
--- SMA-ProjectCompute/src/process/InverseFunction.java
+++ SMA-ProjectCompute/src/process/InverseFunction.java
@@ -1,5 +1,22 @@
 package process;
 
-public class InverseFunction {
+public class InverseFunction extends Function{
 
+	public InverseFunction(double min, double max, double delta) {
+		super(min, max, delta);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public double f(double x) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double eval() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
