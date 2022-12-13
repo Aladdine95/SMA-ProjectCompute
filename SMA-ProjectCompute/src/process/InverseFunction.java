4482854841231fd2857ce8ac56e1bb3a498c0efd
redouanedebart <77845511+redouanedebart@users.noreply.github.com>
2022-12-13 17:51:21 +0100

    Add files via upload
    pb de compil et pour push, il y a surement des erreurs. modif de la classe inverseFunction (methode f(x) et methode de simpson)

--- SMA-ProjectCompute/src/process/InverseFunction.java
+++ SMA-ProjectCompute/src/process/InverseFunction.java
@@ -10,13 +10,44 @@ public class InverseFunction extends Function{
 	@Override
 	public double f(double x) {
 		// TODO Auto-generated method stub
-		return 0;
+		try {
+			return 1/x; //check math lib
+		}catch(numericalException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public double eval() {
+	public double eval() { //methode de simpson ou rectangles ?
 		// TODO Auto-generated method stub
-		return 0;
-	}
+		//rectangles :
+//		float tmp = 0;
+//		float sum = 0;
+//		while(tmp<(this.max-this.min)) {
+//			sum += f(min + tmp) * this.delta;
+//			tmp += this.delta;
+//		}
+//		return sum;
+//	}
+	/*
+	 * simpson method:
+     *double h = (b - a) / (N - 1);     // step size = delta
+     */
+      // 1/3 terms
+      double sum = 1.0 / 3.0 * (f(a) + f(b));
+	 
+      // 4/3 terms
+      for (int i = 1; i < N - 1; i += 2) {
+         double x = a + delta * i;
+         sum += 4.0 / 3.0 * f(x);
+      }
+     	
+      // 2/3 terms
+      for (int i = 2; i < N - 1; i += 2) {
+         double x = a + delta * i;
+         sum += 2.0 / 3.0 * f(x);
+      }
+     return sum * delta;
+	 
 	
 }
